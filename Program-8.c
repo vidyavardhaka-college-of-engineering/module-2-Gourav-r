b3a1c3fbef1c18eb6ce086db64cd4faeee8fbc96
Gourav R <78352781+Gourav-r@users.noreply.github.com>
2021-06-22 17:51:18 +0000

    updt

--- Program-8.c
+++ Program-8.c
@@ -2,13 +2,14 @@
 //OUTPUT - The number of times the  key element  is repeated  is 2
 #include<stdio.h>
 int main(){
-  int n,i,key,a[10],c=0;
+  int n,flag=0,i,key,a[10],c=0;
   printf("Enter size of the array,  array elemnts and the key");
   scanf("%d",&n);
   //printf("Enter array elements");
-  for(i=0;i<n;i++){//missing code
-  scanf("%d",&a[i]);}//missing code
-  //printf("Enter an integer value : ");
+  for(i=0;i<n;i++)
+  {
+  scanf("%d",&a[i]);
+  }//printf("Enter an integer value : ");
   scanf("%d",&key);
   
     for(i=0;i<n;i++){
@@ -21,7 +22,7 @@ int main(){
    printf("The number of times the  key element  is repeated  is %d",c);//missing code
   
     
-    return(0);
+    return 0;
 }
 
 
